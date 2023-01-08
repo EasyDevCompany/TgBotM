33c155b937ae6e85917abc648a4e11fc6cf8b22f
EasyDevCompany <EasyDevv@yandex.ru>
2023-01-09 02:59:58 +0300

    add permissions

--- app/app/admin/views/base.py
+++ app/app/admin/views/base.py
@@ -1,16 +1,23 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
+from flask import abort
 from app.models.user import User
 from loguru import logger
 
 
 class AdminModelView(ModelView):
+
     def is_accessible(self):
         user = current_user
-        if user.role == User.UsersRoles.admin:
+        if user.role == User.UsersRoles.super_user:
             return user.is_authenticated
 
 
 class CustomModelView(ModelView):
+
     def is_accessible(self):
         return current_user.is_authenticated
+
+
+
+
